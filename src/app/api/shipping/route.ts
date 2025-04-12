3c1d9f93d9876757b84ee948a3185b9d25360abc
sahiru <sahiruwrcg@gmail.com>
2025-04-12 21:36:00 +0530

    Add API route for updating shipping locations and enhance shipping data management page

---
+++ src/app/api/shipping/route.ts
@@ -0,0 +1,39 @@
+import { createClient } from "@/utils/supabase/supabase";
+
+
+export async function PUT(req: Request) {
+  const supabaseClient = await createClient();
+
+  const body = await req.text();
+  const { payload } = JSON.parse(body);
+  console.log("Payload", payload);
+  const { data, error } = await supabaseClient.from("shipinfo").upsert(payload);
+
+  if (error) {
+    return new Response(
+      JSON.stringify({
+        message: "An error occurred while updating location",
+        error: error.message,
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return new Response(
+    JSON.stringify({
+      message: "Location updated successfully",
+      data,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+} 
