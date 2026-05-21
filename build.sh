9756ec17362baee4d7643889ecf5c8d61bd51bf4
sahiru <sahiruwrcg@gmail.com>
2026-05-21 18:47:34 +0530

    feat: add pm2 start command to build script to ensure process initialization

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+pm2 start npm --name int-admin -- start
 pm2 restart int-admin --update-env                                                                                                                                        
